ac5903379d05c44505c8b4ef1ddc30b57ca1fee8
Roman Orlovsky <pub@sunneh.ru>
2021-02-22 00:09:29 +0300

    fix: server is read-only now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ readOnly: true });
 
 server.use(middlewares);
 
