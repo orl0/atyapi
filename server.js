f5fc21fbea8cfc94ddb96a6c8b36de5ca07c64f4
Roman Orlovsky <pub@sunneh.ru>
2021-02-21 17:23:15 +0300

    chore: remove unused commented code

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-// server.get("/api/v2", (req, res) => {
-//   res.jsonp(req.query);
-// });
-
 server.use("/api/v2", router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
