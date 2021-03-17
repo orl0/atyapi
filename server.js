8d52afe516cfb422dd667e32645865cda74c2d1e
Roman Orlovsky <pub@sunneh.ru>
2021-03-18 00:17:00 +0300

    feat!: started to work on auto-updating server
    db is loaded via async adapter now

--- server.js
+++ server.js
@@ -1,12 +1,70 @@
-const path = require("path");
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults({ readOnly: true });
+require("dotenv").config();
 
-server.use(middlewares);
+const gatherAPIBaseUrl = process.env.API_BASE_URL;
+if (!gatherAPIBaseUrl) throw new Error("API_BASE_URL env is required!");
 
-server.use("/api/v2", router);
-server.listen(9000, () => {
-  console.log("JSON Server is running");
+const dbFile = require("path").join(__dirname, "db.json");
+
+const updateDatabase = require("./updater");
+
+const low = require("lowdb");
+const FileAsync = require("lowdb/adapters/FileAsync");
+class myFileAsync extends FileAsync {
+  constructor(...args) {
+    super(...args);
+  }
+  read() {
+    console.log("FileAsync read called");
+    return super.read();
+  }
+  write(data) {
+    console.log("FileAsync write called");
+    return super.write(data);
+  }
+}
+const adapter = new myFileAsync(dbFile, {
+  serialize: (data) => JSON.stringify(data, null, 2), // 2 spaces for debug
 });
+const dbPromise = low(adapter);
+
+dbPromise.then((db) => {
+  const lastUpdate = new Date(db.get("lastUpdate").defaultTo(0).value());
+  console.log(`Last update time: ${lastUpdate}`);
+
+  // const oneDay = 1000 * 60 * 60 * 24;
+  const oneDay = 1000 * 60; // 1 min for debug
+
+  if (Date.now() > +lastUpdate + oneDay) {
+    console.log(`Updating...`);
+    updateDatabase(gatherAPIBaseUrl).then((data) => {
+      db.setState(data).write();
+      console.log(`'${adapter.source}' is successfully updated!`);
+    });
+  } else {
+    console.log(`Update is not required...`);
+    // startJSONServer(db);
+  }
+});
+
+function startJSONServer(db) {
+  const jsonServer = require("json-server");
+  const server = jsonServer.create();
+  const router = jsonServer.router(db);
+  const middlewares = jsonServer.defaults({ readOnly: true });
+
+  server.use(middlewares);
+
+  server.use((req, res, next) => {
+    res.header("Last-Modified", lastUpdate.toUTCString());
+
+    if (Date.now() > +lastUpdate + oneDay) console.log(`Update is needed!`);
+
+    next();
+  });
+
+  server.use("/api/v2", router);
+
+  server.listen(9000, () => {
+    console.log("JSON Server is running");
+  });
+}
